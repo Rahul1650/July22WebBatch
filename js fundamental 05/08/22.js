d0c5838c01e1eb9d8eb38bc705ed0a3eda56fda3
Rahul1650 <102297150+Rahul1650@users.noreply.github.com>
2022-08-04 22:32:28 +0530

    Add files via upload

---
+++ js fundamental 05/08/22.js
@@ -0,0 +1,15 @@
+// primitive data
+var name="rahul"
+console.log(name)
+console.log(".................")
+// non primitive data
+var studentDetails={
+    name:"rahul kumar",
+    id:17034,
+    contactnumber:9415345479
+};
+console.log(studentDetails)
+// now name is a key and rahul kumar is value and it is total varibales to store  a data
+//  now update the value
+studentDetails.contactnumber=9415345479
+console.log(studentDetails)
